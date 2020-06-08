2dd4cba70f2ace262d83963801bb1df3aed23193
am-MongoDB <andrew.morgan@mongodb.com>
2020-06-08 18:12:03 +0100

    Decouple from cloud-dev

--- o-fish-ios/Helpers/Constants.swift
+++ o-fish-ios/Helpers/Constants.swift
@@ -11,8 +11,5 @@ struct Constants {
 
     static let realmAppId = "your_app_id"
 
-    // TODO: Remove when we move out of the cloud-dev environment
-    static let realmURL = "https://realm-dev.mongodb.com"
-
     static let DefaultLocation = CLLocationCoordinate2D(latitude: 51.506520923981554, longitude: -0.10689139236939127)
 }
