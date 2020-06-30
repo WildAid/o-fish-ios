516b5bfa551f5f581aab4c0c351b8c94ee29841b
Artem_Kutasevych@epam.com <Artem_Kutasevych@epam.com>
2020-06-30 15:21:17 +0300

    Latitude and longitude precision [-] Fixed coordinates to format N53°52'25" [-] Fixed background and size for MinusIconView [-] Changed foreground color for addAttachmentIcon and EditIcon

--- o-fish-ios/Views/Components/MinusIconView.swift
+++ o-fish-ios/Views/Components/MinusIconView.swift
@@ -10,20 +10,28 @@ import SwiftUI
 struct MinusIconView: View {
 
     private enum Dimensions {
-        static let padding: CGFloat = 12
-        static let imageSize: CGFloat = 16.0
+        static let padding: CGFloat = 3.0
+        static let imageSize: CGFloat = 20.0
+        static let cirleSize: CGFloat = 15.0
     }
 
     var body: some View {
-        Image(systemName: "minus.circle.fill")
-            .font(.system(size: Dimensions.imageSize))
-            .foregroundColor(Color.red)
-            .padding(.top, Dimensions.padding)
+        ZStack {
+            Circle()
+                .fill(Color.white)
+                .frame(width: Dimensions.cirleSize, height: Dimensions.cirleSize)
+
+            Image(systemName: "minus.circle.fill")
+                .font(.system(size: Dimensions.imageSize))
+                .foregroundColor(Color.red)
+        }
+            .padding(.vertical, Dimensions.padding)
     }
 }
 
 struct MinusIconView_Previews: PreviewProvider {
     static var previews: some View {
         MinusIconView()
+            .background(Color.black)
     }
 }
