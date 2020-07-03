818ca7300553ca9d551e6d8cf0de498b988798bd
kojiba <kojiba@ro.ru>
2020-07-03 17:07:12 +0300

    Added DutyReportsView when going off duty

---
+++ o-fish-ios/Views/PatrolBoat/DutyReportsView.swift
@@ -0,0 +1,57 @@
+//
+//  DutyReportsView.swift
+//
+//  Created on 2/06/20.
+//  Copyright © 2020 WildAid. All rights reserved.
+//
+
+import SwiftUI
+
+struct DutyReportsView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @State var dutyReports: [ReportViewModel] = []
+    @ObservedObject var onDuty: DutyState
+
+    @State private var showingBoardingRecordView = false
+    @State private var selectedReport = ReportViewModel()
+
+    private let snapshotManager = LocationSnapshotManager()
+
+    var body: some View {
+        VStack(spacing: .zero) {
+            ScrollView {
+                wrappedShadowView {
+                    VStack(spacing: .zero) {
+                        ForEach(self.dutyReports) { report in
+
+                            VesselRecordItemView(report: report,
+                                snapshotManager: self.snapshotManager)
+                                .onTapGesture {
+                                    self.openBoardingRecordView(with: report)
+                                }
+                        }
+                    }
+                }
+            }
+
+            NavigationLink(destination: BoardingRecordView(report: selectedReport), isActive: $showingBoardingRecordView) {
+                EmptyView()
+            }
+        }
+            .background(Color.backgroundGrey)
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationBarTitle("Duty Reports", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                BackButton(label: "Back")
+            })
+    }
+
+    /// Actions
+
+    private func openBoardingRecordView(with report: ReportViewModel) {
+        selectedReport = report
+        showingBoardingRecordView = true
+    }
+}
