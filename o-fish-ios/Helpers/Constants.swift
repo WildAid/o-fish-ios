e48d04bf6855d9557d27580b4f88ced124b06ee4
kojiba <kojiba@ro.ru>
2020-07-15 17:23:34 +0300

    Added Image Compressing to configured size [-] JPEG compression to 3 MB setup

--- o-fish-ios/Helpers/Constants.swift
+++ o-fish-ios/Helpers/Constants.swift
@@ -13,6 +13,8 @@ struct Constants {
 
     static let DefaultLocation = CLLocationCoordinate2D(latitude: 51.506520923981554, longitude: -0.10689139236939127)
 
+    static let maximumImageSize = 3 * 1024 * 1024 // 3 MB
+
     enum Notifications {
         static let hoursAfterClosing = 4
         static let hoursAfterStarting = 10
