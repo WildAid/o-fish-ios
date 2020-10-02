065259b2fd4db725558568a1717b2f0d768baadc
Andrew Morgan <andrew.morgan@mongodb.com>
2020-10-02 15:15:16 +0100

    Create LocalConstants.swift

---
+++ o-fish-ios/LocalConstants.swift
@@ -0,0 +1,10 @@
+//
+//  LocalConstants.swift
+//  
+//  Created on 29/09/2020.
+//  Copyright © 2020 WildAid. All rights reserved.
+//
+
+struct LocalConstants {
+    static let realmAppId = "your-realm-app-id"
+}
