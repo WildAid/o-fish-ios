d8f2565dd862369533ef199686cb6cdcd7b2a35e
Tim Brooks <bladebunny91@gmail.com>
2020-10-13 10:15:43 -0700

    Revert "Merge branch 'main' of https://github.com/bladebunny/o-fish-ios into 347-darkmode"
    This reverts commit 2e0fe538fe5e9606c51d8bd7d73b3ba48fdb1a73, reversing
    changes made to 02ebc196f5d9d61f2c1c97dc5f61bffbbf8777d1.

--- o-fish-ios/Views/Components/InputField.swift
+++ o-fish-ios/Views/Components/InputField.swift
@@ -13,10 +13,14 @@ struct InputField: View {
 
     var showingSecureField = false
     var showingWarning = false
+
     var captionColor = Color.removeAction
     var separatorColor = Color.inactiveBar
     var warningColor = Color.spanishOrange
+
     var inputChanged: ((String) -> Void)?
+
+    var keyboardType: UIKeyboardType = .alphabet
 
     private enum Dimensions {
         static let noSpacing: CGFloat = 0
@@ -29,21 +33,16 @@ struct InputField: View {
             CaptionLabel(title: title, color: showingWarning ? warningColor : captionColor)
 
             HStack(spacing: Dimensions.noSpacing) {
-                if !showingSecureField {
-                    TextField("", text: textBinding)
-                        .padding(.bottom, Dimensions.bottomPadding)
-                        .foregroundColor(.text)
-                        .font(.body)
-                } else {
-                    SecureField("", text: textBinding)
-                        .padding(.bottom, Dimensions.bottomPadding)
-                        .foregroundColor(.text)
-                        .font(.body)
-                }
+                DoneTextField(text: textBinding, keyboardType: keyboardType, isSecureTextEntry: showingSecureField)
+                    .padding(.bottom, Dimensions.bottomPadding)
+                    .foregroundColor(.text)
+                    .font(.body)
+
                 if showingWarning {
                     ExclamationIconView()
                 }
             }
+
             Divider()
                 .background(showingWarning ? warningColor : separatorColor)
         }
